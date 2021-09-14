7201bb6aaa6e032b52995356415eb4a42d325d86
ayanami <495966854@qq.com>
2021-09-14 21:13:40 +0800

    修改文件

--- sort/src/sort/sort_three.java
+++ sort/src/sort/sort_three.java
@@ -5,37 +5,38 @@ public class sort_three {
 	public static void main(String args[]) {
 		int a[] = new int[10];
 		Scanner scan = new Scanner(System.in);
-		int i,j;
+		int i;
 		for(i=0;i<a.length;i++) {
 			a[i] = scan.nextInt();
-			quicksort(a,0,a.length-1);
 		}
+			quicksort(a,0,a.length-1);
 		for(i=0;i<a.length;i++) {
 			System.out.println(a[i]+" ");
 		}
 	}
 	private static void quicksort(int a[],int left,int right) {
-		if(left<right) {
-			int mid = getmid(a,left,right);
-			quicksort(a,left,mid-1);
-			quicksort(a,mid+1,right);
+		if(left>right) {
+			return;
 		}
+		int i,j,temp,temp1;
+		temp=a[left];
+		i=left;
+		j=right;
+		while(i<j) {
+			while(a[j]>=temp&&i<j) {
+				j--;
+			}
+			while(a[i]<=temp&&i<j) {
+				i++;
+			}
+			temp1=a[j];
+			a[j]=a[i];
+			a[i]=temp1;
+		}
+		a[left]=a[i];
+		a[i]=temp;
+		quicksort(a,left,i-1);
+		quicksort(a,i+1,right);
 	}
-
-    private static int getmid(int a[],int left,int right) {
-	    int temp;
-	    temp=a[left];
-	    while(left<right) {
-		    while(a[right]>=temp&&left<right) {	    	
-			    right--;
-		    }
-		    a[left]=a[right];
-		    while(a[left]<=temp&&left<right) {
-			    left++;
-		    }
-		    a[right]=a[left];
-	    }
-	    a[left]=temp;
-	    return left;
-     }
 }
+
